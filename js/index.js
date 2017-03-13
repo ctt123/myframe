ee9e8023db3b689530937c0a35244fc994661734
Scarlett <283569035@qq.com>
2017-03-13 17:22:55 +0800

    1

---
+++ js/index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by chentingting on 2017/3/10.
+ */
+alert(1)
---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Created by chentingting on 2017/3/10.
+ */
+require.config({
+    baseUrl:"js",
+    paths:{
+        "bootstrapJs":"bootstrap",
+        "index":"index",
+        "angular":"angular",
+
+    }
+})
+require(["index"],function(){
+
+
+})
